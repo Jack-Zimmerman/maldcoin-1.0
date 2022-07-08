f8e156feceb81d4f8fab1bc039a9ce986b3c4d26
Jack Zimmerman <jackzimmerman2006@gmail.com>
2022-07-07 22:42:26 -0700

    renamed blockchain folder to "node-program" to begin construction on node server

--- gui/src/content/Transactions.js
+++ gui/src/content/Transactions.js
@@ -1,9 +1,50 @@
 import React from 'react'
 
+
+
+export class BalanceViewer extends React.Component{
+    constructor(){
+        super()
+        this.state = {balance: 0.0000000};
+        
+    }
+
+    async grabBalanceLoop(){
+        for(;;){
+            //every 5 seconds send request for balance from node
+            await new Promise(resolve => setTimeout(resolve(), 5000))
+
+            //get balance from localhost server /balance
+            const givenBalance = await (await fetch("http://localhost:8080/balance")).json()
+
+            this.setState(
+                { 
+                    balance: givenBalance
+                }
+            )
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <h2>
+                    {
+                        //balance to 8 decimal places(accuracy of COIN)
+                        this.state.balance.toFixed(8)
+                    }
+                </h2>
+            </div>
+        )
+    }
+}
+
+
 export const transactionsPage = ( 
     <>
     <div>
-        <h1 class="px-1 mx-1">Balance:</h1>
+        <h1>Balance:</h1>
+        <BalanceViewer></BalanceViewer>
     </div>
     </>
 ) 
